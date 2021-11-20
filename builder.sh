1e58e900bb56e8e83d7845d288cfaad5d68ff4ca
Kyb3r Vizsla <me@kyb3rvizsla.com>
2021-11-20 16:13:43 -0700

    Update

--- builder.sh
+++ builder.sh
@@ -6,13 +6,33 @@
 # By: Kyb3r Vizsla <kyb3rvizsla.com>
 #---------------------------------
 
-# Variables
-ROOTFS="rootfs"
-DEBIAN_RELEASE="bullseye"
+source example.conf
+source custom.conf 2> /dev/null
 
 # First stage
-eatmydata debootstrap --foreign --arch="arm64" --include="ifupdown openresolv net-tools init dbus rsyslog cron eatmydata wget gnupg" $DEBIAN_RELEASE $ROOTFS
+eatmydata debootstrap --foreign --arch="$architecture" --include="ifupdown openresolv net-tools init dbus rsyslog cron eatmydata wget gnupg" $debian_release $rootfs
 
 # Second stage
-chroot $ROOTFS /debootstrap/debootstrap --second-stage
+chroot $rootfs eatmydata /debootstrap/debootstrap --second-stage
 
+# Set users
+chroot $rootfs <<_EOF
+echo "root:${root_password}" | chpasswd
+_EOF
+
+# Set mounting system files
+cat >$rootfs/etc/fstab <<EOM
+# <file system>   <dir>           <type>  <options>         <dump>  <pass>
+proc              /proc           proc    defaults          0       0
+/dev/mmcblk0p1    /boot           vfat    defaults          0       2
+/dev/mmcblk0p2    /               $fstype    defaults,noatime  0       1
+EOM
+
+# Set hosts file
+cat >$rootfs/etc/hosts <<EOM
+127.0.1.1       ${host_name}
+127.0.0.1       localhost
+::1             localhostnet.ifnames=0 ip6-localhost ip6-loopback
+ff02::1         ip6-allnodes
+ff02::2         ip6-allrouters
+EOM
