7b27a3875a2bf923761decad2d47acff61f7cbad
Kyb3r Vizsla <me@kyb3rvizsla.com>
2021-11-21 14:51:29 -0700

    Colors :D

--- builder.sh
+++ builder.sh
@@ -13,30 +13,41 @@ source custom.conf 2> /dev/null
 base_packages="ca-certificates wget curl gnupg cron rsyslog"
 debootstrap_include_packages="eatmydata gnupg"
 packages="$base_packages $custom_packages"
+# Colors
+endColor="\e[0m\e[0m"
+redColor="\e[0;31m\e[1m"
+blueColor="\e[0;34m\e[1m"
+cyanColor="\e[01;96m\e[1m"
+grayColor="\e[0;37m\e[1m"
+greenColor="\e[0;32m\e[1m"
+purpleColor="\e[0;35m\e[1m"
+yellowColor="\e[0;33m\e[1m"
+turquoiseColor="\e[0;36m\e[1m"
+dot="${redColor}[${endColor}${yellowColor}*${endColor}${redColor}]${endColor}"
 
 # Preparation
 rm -rf $word_dir 2> /dev/null
 mkdir $work_dir
 
 # First stage
-debootstrap --foreign --arch="$architecture" --include="$debootstrap_include_packages" $debian_release $rootfs
+echo -e "\n$dot$greenColor Starting first stage...$endColor"
+eatmydata debootstrap --foreign --arch="$architecture" --include="$debootstrap_include_packages" $debian_release $rootfs
+echo "$(date +"DAY: %d MONTH: %b HOUR: %I MINUTE: %M SECOND: %S")" > $work_dir/build-date.txt
 
 # Second stage
+echo -e "\n$dot$greenColor Starting second stage...$endColor"
 cp /usr/bin/qemu-aarch64-static $rootfs/usr/bin # copy qemu bin for chroot
-chroot $rootfs /debootstrap/debootstrap --second-stage
+chroot $rootfs eatmydata /debootstrap/debootstrap --second-stage
 chroot $rootfs apt update
 
 # Install packages
 chroot $rootfs <<_EOF
+echo -e "\n$dot$green Installing packages...$endColor"
 apt install -y $packages
 _EOF
 
-# Set users
-chroot $rootfs <<_EOF
-echo "root:${root_password}" | chpasswd
-_EOF
-
 # Set mounting system files
+echo -e "\n${yellowColor}Setting mounting systm files...$endColor"
 cat >$rootfs/etc/fstab <<EOM
 # <file system>   <dir>           <type>  <options>         <dump>  <pass>
 proc              /proc           proc    defaults          0       0
@@ -44,16 +55,8 @@ proc              /proc           proc    defaults          0       0
 /dev/mmcblk0p2    /               $fstype    defaults,noatime  0       1
 EOM
 
-# Set hosts file
-cat >$rootfs/etc/hosts <<EOM
-127.0.1.1       ${host_name}
-127.0.0.1       localhost
-::1             localhostnet.ifnames=0 ip6-localhost ip6-loopback
-ff02::1         ip6-allnodes
-ff02::2         ip6-allrouters
-EOM
-
 # Configure networking
+echo -e "\n${yellowColor}Configuring netorking...$endColor"
 chroot $rootfs apt install -y resolvconf
 chroot $rootfs systemctl enable resolvconf
 chroot $rootfs systemctl enable NetworkManager
@@ -69,7 +72,31 @@ nameserver 8.8.8.8
 #nameserver 1.1.1.1
 EOM
 
+# Setting...
+echo -e "\n$dot$greenColor Starting settings...$endColor"
+
+# Set hosts files
+echo -e "\n${yellowColor}Setting hosts...$endColor"
+cat >$rootfs/etc/hosts <<EOM
+127.0.1.1       ${host_name}
+127.0.0.1       localhost
+::1             localhostnet.ifnames=0 ip6-localhost ip6-loopback
+ff02::1         ip6-allnodes
+ff02::2         ip6-allrouters
+EOM
+rm $rootfs/hostname
+cat >$rootfs/etc/hotname <<EOM
+${host_name}
+EOM
+
+# Set users
+echo -e "${yellowColor}Setting users$endColor"
+chroot $rootfs <<_EOF
+echo "root:${root_password}" | chpasswd
+_EOF
+
 # Set timezone
+echo -e "${yellowColor}Setting timezone$endColor"
 apt install -y tzdata
 chroot $rootfs <<_EOF
 ln -nfs /usr/share/zoneinfo/$timezone /etc/localtime
@@ -77,6 +104,7 @@ dpkg-reconfigure -fnoninteractive tzdata
 _EOF
 
 # Set locales
+echo -e "${yellowColor}Setting locales$endColor"
 apt install -y locales
 sed -i "s/^# *\($locale\)/\1/" $rootfs/etc/locale.gen
 chroot $rootfs locale-gen
@@ -89,6 +117,7 @@ fi
 EOM
 
 # Install kernel
+echo -e "\n$dot$greenColor Installing kernel...$endColor"
 chroot $rootfs apt install -y curl
 wget https://raw.githubusercontent.com/raspberrypi/rpi-update/master/rpi-update -O $rootfs/usr/local/sbin/rpi-update
 chmod +x $rootfs/usr/local/sbin/rpi-update
